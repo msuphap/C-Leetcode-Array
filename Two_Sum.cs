53670e0b1daef723e86f468d04d2e6b878dbdd8a
MUCHCHIMA <22536728+msuphap@users.noreply.github.com>
2018-03-24 19:20:39 -0500

    Create Two_Sum.cs

---
+++ Two_Sum.cs
@@ -0,0 +1,31 @@
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// Example:
+// Given nums = [2, 7, 11, 15], target = 9,
+// Because nums[0] + nums[1] = 2 + 7 = 9,
+// return [0, 1].
+
+
+
+public class Solution {
+    
+    public int[] TwoSum(int[] nums, int target) {
+        
+       var dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var diff = target - nums[i];
+                if(dict.ContainsKey(diff))
+                {
+                    return new int[] { dict[diff], i };
+                }
+                else
+                {
+                    if(!dict.ContainsKey(nums[i]))
+                        dict.Add(nums[i], i);
+                }
+            }
+
+            return null;
+    }
+}
